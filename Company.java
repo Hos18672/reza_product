9f4e0071d8c1f84c6390e8cb40fecc3277a6da0e
Rezaafg <rezahosseiniafg@gmail.com>
2020-04-05 17:01:22 +0200

    Add age

---
+++ Company.java
@@ -0,0 +1,23 @@
+
+public class Product {
+
+    private String name = "Coca Cola2";
+    private int Age= 20; 
+
+    public Product(String name)
+    {
+        setName(name);
+
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+}
