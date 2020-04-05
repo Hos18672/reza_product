6ff692bf82087e55ba90e8dd16eb58afdfc2f2cc
Reza <hos18672@spengergasse.at>
2020-04-05 15:04:51 +0200

    Create Product Class

---
+++ Product.java
@@ -0,0 +1,22 @@
+
+public class Product {
+
+    private String name = "Coca Cola2";
+
+    public Product(String name)
+    {
+        setName(name);
+
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+}
