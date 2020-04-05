3ae2484ad0e6b654dc0ff0e3a1001d054d1c6ed0
Naghmeh19 <sal19867@spengergasse.at>
2020-04-05 21:19:29 +0200

    onStock to Product

--- Product.java
+++ Product.java
@@ -1,29 +1,33 @@
 
 public class Product {
     private String name = "Coca Cola2";
-    private onStock: boolean = true;
+private onStock: boolean = true;
+    
    private float price = 10;
     public Product(String name)
     {
         setName(name);
-	setOnStock(true);
-
+        setOnStock(true);
+	
     }
 
     public void setName(String name)
     {
         this.name=name;
     }
-    public void setsetOnStock(boolean onStock)
+public void setsetOnStock(boolean onStock)
    {
 	this.onStock=onStock;
    }
+
+    
     public String getName()
     {
         return name;
     }
-    public boolean getOnStock()
+public boolean getOnStock()
    {
       return onStock;
    }
+    
 }
